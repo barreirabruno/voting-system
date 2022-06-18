3415e367680e1179149c18ec96b118296e1c1bce
brunobarreira <brunobarreirasilva@gmail.com>
2022-06-17 23:28:46 -0300

    refactor: remove Election getters and setters

--- src/domain/entities/Election.domain.ts
+++ src/domain/entities/Election.domain.ts
@@ -1,11 +1,12 @@
 import { randomUUID } from 'crypto'
 import { ElectionStatusEnum } from './ElectionStatusEnum.domain';
+import { addDaystoDate } from './utils/date-plus-five-days';
 
 type ElectionData = {
   electionStatus?: ElectionStatusEnum
   electionTitleParam: string
-  startDateElectionParam: Date
-  endDateElectionParam: Date
+  startDateElectionParam?: Date
+  endDateElectionParam?: Date
 }
 
 export default class Election {
@@ -20,44 +21,8 @@ export default class Election {
     this.id = randomUUID();
     this.status = ElectionStatusEnum.CREATED ?? electionDataParams.electionStatus;
     this.title = electionDataParams.electionTitleParam;
-    this.start = electionDataParams.startDateElectionParam;
-    this.end = electionDataParams.endDateElectionParam;
+    this.start = electionDataParams.startDateElectionParam ?? new Date(Date.now());
+    this.end = electionDataParams.endDateElectionParam ?? addDaystoDate(5, new Date());
     this.createdAt = new Date(Date.now());
-  } 
-
-  getId(): string {
-    return this.id
-  }
-
-  getStatus(): string {
-    return this.status
-  }
-
-  setStatus(status: ElectionStatusEnum): void {
-    this.status = status
-  }
-
-  getTitle(): string {
-    return this.title
-  }
-
-  setTitle(title: string): void {
-    this.title = title
-  }
-
-  getStart(): Date {
-    return this.start
-  }
-
-  setStart(startDate: Date): void {
-    this.start = startDate
-  }
-
-  getEnd(): Date {
-    return this.end
-  }
-
-  setEnd(endDate: Date): void {
-    this.end = endDate
   }
 }

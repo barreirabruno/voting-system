547b784b0ad88aa3fc7528c9efa4c9fb1907962d
brunobarreira <brunobarreirasilva@gmail.com>
2022-06-19 13:06:58 -0300

    feat: add CreateElectionController

---
+++ src/application/controllers/election-controller.ts
@@ -0,0 +1,28 @@
+import { ElectionService } from "@/data/services/election.service";
+import { ElectionStatusEnum } from "@/domain/entities";
+import { HttpResponse, ok } from "../helpers/http";
+import { Controller } from "./controller";
+
+type HttpRequest = {
+  title: string,
+  start: Date,
+  end: Date
+}
+
+export class CreateElectionController extends Controller {
+  constructor(
+    private readonly electionService: ElectionService
+  ) {
+    super()
+  }
+
+  async perform(httpRequest: any): Promise<HttpResponse<any>> {
+    const result = await this.electionService.perform({
+      electionStatus: httpRequest.electionStatus,
+      electionTitleParam: httpRequest.electionTitleParam,
+      startDateElectionParam: httpRequest.startDateElectionParam,
+      endDateElectionParam: httpRequest.endDateElectionParam
+    })
+    return ok(result);
+  }
+}
